f7da802674d6e2627df6f3353739db7bbdb77219
sansabando <113368967+sansabando@users.noreply.github.com>
2024-10-13 20:19:16 +0000

    dinamic title

--- script.js
+++ script.js
@@ -4,3 +4,42 @@ let operator = '';
 let csvValues = []; 
 let errorLog = []; 
 let operationLog = []; 
+
+//Actualize the value and dinamic title
+function updateDisplay() {
+    const display = document.getElementById("display");
+    display.value = currentValue;
+
+    
+    const progressBar = document.getElementById("progress-bar");
+    progressBar.classList.add('active');
+    setTimeout(() => {
+        progressBar.classList.remove('active');
+    }, 2000);
+    updateDynamicTitle(currentValue);
+}
+
+
+
+function updateDynamicTitle(value) {
+    const titleElement = document.getElementById("info");
+    
+    if (value === '') {
+        titleElement.innerHTML = 'Information about the Number'; 
+    } else {
+        let operationText = `Operation: ${operator}`; 
+
+        let resultText;
+
+        
+        if (value < 100) {
+            resultText = 'Result: The result is less than 100';
+        } else if (value >= 100 && value <= 200) {
+            resultText = 'Result: The result is between 100 and 200';
+        } else {
+            resultText = 'Result: The result is greater than 200';
+        }
+
+        titleElement.innerHTML = `${operationText}<br>${resultText}`; 
+    }
+}
